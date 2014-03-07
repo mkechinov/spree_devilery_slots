791aef0cb45e6db9282c5a59177c34eb54c7a9bd
Anton Smirnov <anton.smirnov@mkechinov.ru>
2014-03-07 14:07:06 +0400

    * reservation vlidations * get / clean current

--- app/models/spree/delivery_slot_reservation.rb
+++ app/models/spree/delivery_slot_reservation.rb
@@ -3,6 +3,10 @@ module Spree
     belongs_to :user
     belongs_to :delivery_slot
 
+    validates :delivery_date, presence: true
+    validates :expires_at,    presence: true
+    validates :delivery_slot, presence: true
+
     after_initialize :after_initialize
 
     scope :expired, -> { where('expires_at < ?',  Time.now) }
@@ -18,6 +22,23 @@ module Spree
 
     def self.clean_expired
       self.expired.destroy_all
+    end
+
+    def self.get_current
+      if spree_current_user && spree_current_user.delivery_slot_reservation && spree_current_user.delivery_slot_reservation.expired? == false
+        return spree_current_user.delivery_slot_reservation
+      end
+
+      if session && session[:session_id]
+        return self.active.where(user_session: session[:session_id]).last
+      end
+
+      nil
+    end
+
+    def self.destroy_current
+        spree_current_user.delivery_slot_reservation.destroy       if spree_current_user && spree_current_user.delivery_slot_reservation
+        self.where(user_session: session[:session_id]).destroy_all if session && session[:session_id]
     end
   end
 end
